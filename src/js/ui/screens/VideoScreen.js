3023f89d28ec61309e2b4b50d9f914b2147ae504
ialkhasov <mr.ilyass@gmail.com>
2021-11-18 23:15:33 +0300

    video playback controls added

--- src/js/ui/screens/VideoScreen.js
+++ src/js/ui/screens/VideoScreen.js
@@ -7,26 +7,28 @@ export default class VideoScreen extends React.Component {
 
     render() {
         let {item} = this.props.route.params
-        
+
         let source = core.createReactSource(item)
-        
+
         console.log('source: ' + JSON.stringify(source, null, 2))
-        
+
         return <SafeAreaView style={{flex:1}}>
-            <Video source={source} 
+            <Video
+                source={source}
+                controls={true}
                 style={{
                     position: 'absolute',
                     top: 0,
                     left: 0,
                     bottom: 0,
                     right: 0,
-                }} 
+                }}
                 ref={(ref) => {
                     this.player = ref
                 }}                                      // Store reference
                 onBuffer={(a,b)=>console.log('on buffer: ' + JSON.stringify(a) + ' ' + + JSON.stringify(b))}
                 onError={(a,b)=>console.log('on error: ' + JSON.stringify(a) + ' ' + + JSON.stringify(b))}
-                
+
             />
         </SafeAreaView>
     }

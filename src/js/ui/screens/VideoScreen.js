b8034cbdd2b29ee6884138d1b5c9ca33e03110a1
ialkhasov <mr.ilyass@gmail.com>
2021-11-27 22:33:11 +0300

    minor fixes

--- src/js/ui/screens/VideoScreen.js
+++ src/js/ui/screens/VideoScreen.js
@@ -1,37 +1,54 @@
 import * as React from 'react';
-import { SafeAreaView } from 'react-native';
+import {SafeAreaView} from 'react-native';
 import core from '../../core';
 import Video from 'react-native-video';
+import LoadingView from "../components/LoadingView";
+import {observer} from "mobx-react";
 
+const style = {
+  safeArea: {
+    style: { flex: 1 }
+  },
+}
+@observer
 export default class VideoScreen extends React.Component {
 
-    render() {
-        let {item} = this.props.route.params
+  constructor() {
+    super();
+    this.state = {}
+  }
 
-        let source = core.createReactSource(item)
+  async componentDidMount() {
+    let {item} = this.props.route.params;
+    let source = core.createReactSource(item);
+    this.setState({ source });
 
-        console.log('source: ' + JSON.stringify(source, null, 2))
+    console.log('source: ' + JSON.stringify(source, null, 2))
+  }
 
-        return <SafeAreaView style={{flex:1}}>
-            <Video
-                source={source}
-                resizeMode={"contain"}
-                controls={true}
-                style={{
-                    position: 'absolute',
-                    top: 0,
-                    left: 0,
-                    bottom: 0,
-                    right: 0,
-                }}
-                ref={(ref) => {
-                    this.player = ref
-                }}                                      // Store reference
-                onBuffer={(a,b)=>console.log('on buffer: ' + JSON.stringify(a) + ' ' + + JSON.stringify(b))}
-                onError={(a,b)=>console.log('on error: ' + JSON.stringify(a) + ' ' + + JSON.stringify(b))}
-
-            />
-        </SafeAreaView>
+  render() {
+    if (!this.state.source) {
+      return <LoadingView />
     }
 
+    return <SafeAreaView style={style.safeArea.style}>
+      <Video
+          source={this.state.source}
+          resizeMode={"contain"}
+          controls={true}
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            bottom: 0,
+            right: 0,
+          }}
+          ref={(ref) => {
+            this.player = ref
+          }}
+          onBuffer={(a,b)=>console.log('on buffer: ' + JSON.stringify(a) + ' ' + + JSON.stringify(b))}
+          onError={(a,b)=>console.log('on error: ' + JSON.stringify(a) + ' ' + + JSON.stringify(b))}
+      />
+    </SafeAreaView>
+  }
 }

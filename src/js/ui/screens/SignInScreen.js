eedd66b2bd7707ad86f16d20a802d5fb82479bbe
ialkhasov <mr.ilyass@gmail.com>
2021-12-24 14:43:40 +0300

    ML-40 fixed

--- src/js/ui/screens/SignInScreen.js
+++ src/js/ui/screens/SignInScreen.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View } from 'react-native';
+import {Keyboard, TouchableWithoutFeedback, View} from 'react-native';
 import styles from '../styles';
 import core from '../../core';
 import { observer } from 'mobx-react';
@@ -7,6 +7,8 @@ import FormTextInput from '../components/FormTextImput';
 import SquareButton from '../components/SquareButton';
 import ErrorView from '../components/ErrorView';
 import ClearTextButton from "../components/ClearTextButton";
+import KeyboardAvoidingView
+    from "react-native/Libraries/Components/Keyboard/KeyboardAvoidingView";
 
 @observer
 export default class SignInScreen extends React.Component {
@@ -31,21 +33,31 @@ export default class SignInScreen extends React.Component {
     }
 
     render() {
-        return <View style={styles.theme.root}>
-            <View style={styles.theme.form.view}>
-                {core.state.authErrorMsg && <ErrorView message={core.state.authErrorMsg}/>}
-                <FormTextInput placeholder={"Email"} value={this.state.email} autoCapitalize='none' autoComplete='off' onChangeText = {(email) => this.setState({...this.state, email})}/>
-                <FormTextInput placeholder={"Password"} value={this.state.password} autoCapitalize='none' autoComplete='off' onChangeText = {(password) => this.setState({...this.state, password})}/>
-                <SquareButton
-                    title="Sign In"
-                    onPress={() => this.onSignIn()}
-                    color={styles.theme.button.color}
-                />
-                <ClearTextButton
-                    title="Sign Up"
-                    onPress={() => this.onSignUp()}
-                />
-            </View>
-        </View>
+        return <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <KeyboardAvoidingView style={styles.theme.root}>
+                <View style={styles.theme.form.view}>
+                    {core.state.authErrorMsg && <ErrorView message={core.state.authErrorMsg}/>}
+                    <FormTextInput
+                        placeholder={"Email"}
+                        value={this.state.email}
+                        autoCapitalize='none' autoComplete='off'
+                        onChangeText = {(email) => this.setState({...this.state, email})}/>
+                    <FormTextInput
+                        placeholder={"Password"}
+                        value={this.state.password}
+                        autoCapitalize='none' autoComplete='off'
+                        onChangeText = {(password) => this.setState({...this.state, password})}/>
+                    <SquareButton
+                        title="Sign In"
+                        onPress={() => this.onSignIn()}
+                        color={styles.theme.button.color}
+                    />
+                    <ClearTextButton
+                        title="Sign Up"
+                        onPress={() => this.onSignUp()}
+                    />
+                </View>
+            </KeyboardAvoidingView>
+        </TouchableWithoutFeedback>
     }
 }
